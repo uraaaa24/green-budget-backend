31ba0c843de02225b895d2a95c58e94feabf4ec8
Yuto Urashima <yuto.urashima24@gmail.com>
2024-12-22 01:54:59 +0900

    feat: add configuration management with Pydantic and update database connection settings

--- app/infrastructure/db/base.py
+++ app/infrastructure/db/base.py
@@ -1,11 +1,22 @@
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine, text
+from app.core.config import settings
 
-DATABASE_URL = "postgresql+psycopg2://postgres:postgres@db:5432/postgres"
 
-engine = create_engine(DATABASE_URL)
+def get_database_url() -> str:
+    """
+    Construct the database URL based on settings.
+    """
+    return (
+        f"postgresql+psycopg2://"
+        f"{settings.postgres_user}:{settings.postgres_password}@"
+        f"{settings.postgres_host}:{settings.postgres_port}/"
+        f"{settings.postgres_db}"
+    )
 
+
+engine = create_engine(get_database_url())
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
