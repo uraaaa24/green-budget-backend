305b758731d6707decc6730cd524da42d9f01e44
Yuto Urashima <yuto.urashima24@gmail.com>
2024-12-28 13:56:42 +0900

    feat: add health check endpoint with response model for service health validation

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
-from fastapi import Depends, FastAPI
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import FastAPI
+from pydantic import BaseModel
 from app.presentation.routers import router as api_router
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -17,6 +17,26 @@ app.add_middleware(
 app.include_router(api_router)
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+class HealthCheck(BaseModel):
+    """Response model to validate and return when performing a health check."""
+
+    status: str = "OK"
+
+
+@app.get(
+    "/",
+    tags=["healthcheck"],
+    summary="Perform a Health Check",
+    response_model=HealthCheck,
+)
+def get_health() -> HealthCheck:
+    """
+    ## Perform a Health Check
+    Endpoint to perform a healthcheck on. This endpoint can primarily be used Docker
+    to ensure a robust container orchestration and management is in place. Other
+    services which rely on proper functioning of the API service will not deploy if this
+    endpoint returns any other HTTP status code except 200 (OK).
+    Returns:
+        HealthCheck: Returns a JSON response with the health status
+    """
+    return HealthCheck(status="OK")
